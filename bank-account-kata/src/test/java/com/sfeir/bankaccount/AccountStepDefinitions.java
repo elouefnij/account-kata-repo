9340313c1dcbbbdb7b00ec7289c522255929dd4c
Jamila EL OUEFNI <elouefni.j@sfeir.com>
2023-09-19 17:03:33 +0200

    US2 - Passing E2E test

--- bank-account-kata/src/test/java/com/sfeir/bankaccount/AccountStepDefinitions.java
+++ bank-account-kata/src/test/java/com/sfeir/bankaccount/AccountStepDefinitions.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertEquals;
 import com.sfeir.bankaccount.business.Account;
 import com.sfeir.bankaccount.business.Amount;
 import com.sfeir.bankaccount.business.Balance;
+import com.sfeir.bankaccount.business.UnauthorizedWithdrawalException;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class AccountDepositStepDefinitions {
+public class AccountStepDefinitions {
 	Account account;
 
 	@Given("Client have an account with {string} balance")
@@ -28,4 +29,14 @@ public class AccountDepositStepDefinitions {
 		Balance expected_balance = Balance.valueOf(balance);
 		assertEquals(expected_balance, account.balance());
 	}
+
+	@When("Client makes a withdrawal of {string}")
+	public void client_makes_a_withdrawal_of(String amount) {
+		try {
+			account.withdraw(Amount.valueOf(amount));
+		} catch (UnauthorizedWithdrawalException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- bank-account-kata/src/test/resources/com/sfeir/bankaccount/features/account_withdrawal.feature
+++ bank-account-kata/src/test/resources/com/sfeir/bankaccount/features/account_withdrawal.feature
@@ -3,9 +3,9 @@ Feature: Account withdrawal
 	In order to retrieve some or all of my savings as a bank client I want to make a withdrawal from my account
 
   Scenario: As a bank client I can retrieve some or all of my savings
-    Given Client have an account with "100" in balance
-    When Client makes a withdrawal of "60"
-    Then The account balance shoud be "40"
+    Given Client have an account with "100.0" balance
+    When Client makes a withdrawal of "60.0"
+    Then The account balance shoud be "40.0"
       
 
     
