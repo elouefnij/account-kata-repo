ca672c6f6f161e9e0874d0989b2b4d5bccd72686
Jamila EL OUEFNI <elouefni.j@sfeir.com>
2023-09-20 14:25:35 +0200

    US3 - UT2 - Should new statement be added when withdrawal

--- bank-account-kata/src/test/java/com/sfeir/bankaccount/business/StatementTest.java
+++ bank-account-kata/src/test/java/com/sfeir/bankaccount/business/StatementTest.java
@@ -2,22 +2,31 @@ package com.sfeir.bankaccount.business;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.function.Supplier;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.sfeir.bankaccount.business.Operation.OperationType;
 
 public class StatementTest {
 
+	Supplier<LocalDateTime> date_supplier;
+
+	@Before
+	public void setup() {
+		date_supplier = () -> {
+			return LocalDateTime.of(2023, 9, 01, 10, 00);
+		};
+	}
+
 	@Test
 	public void should_new_operation_be_added_when_deposit() {
 		//
-		Supplier<LocalDateTime> date_supplier = () -> {
-			return LocalDateTime.of(2023, 9, 01, 10, 00);
-		};
 		StatementLine expected_sl = new StatementLine(
 				new Operation(OperationType.DEPOSIT, date_supplier.get(), Amount.valueOf("100")),
 				Balance.valueOf("100"));
@@ -30,4 +39,18 @@ public class StatementTest {
 		assertEquals(expected_sl.operation(), account.statement().lines().get(0).operation());
 	}
 
+	@Test
+	public void should_new_operation_be_added_when_withdrawal() throws UnauthorizedWithdrawalException {
+		//
+		StatementLine expected_sl = new StatementLine(
+				new Operation(OperationType.WITHDRAWAL, date_supplier.get(), Amount.valueOf("20")),
+				Balance.valueOf("130"));
+		//
+		Account account = new Account(date_supplier, Balance.valueOf("150"));
+		account.withdraw(Amount.valueOf("20"));
+		//
+		assertNotNull(account.statement().lines().get(0));
+		assertEquals(expected_sl.balance(), account.statement().lines().get(0).balance());
+		assertEquals(expected_sl.operation(), account.statement().lines().get(0).operation());
+	}
 }
